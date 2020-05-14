a549eca238a5bd819703981f85788221f285166f
Maicol <maicolbattistini@live.it>
2020-05-14 12:01:45 +0200

    Update githubmd.user.js

--- githubmd.user.js
+++ githubmd.user.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  Extra features to the Github Material Design theme
 // @author       maicol07
+// @icon         https://github.githubassets.com/pinned-octocat.svg
 // @match        https://github.com/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.0/jquery.min.js
@@ -13,21 +14,18 @@
     'use strict';
 
     // INPUTS CLASS INJECTION
-    /*$('input.input-contrast').addClass('form-field__input');
-    $('textarea.input-contrast').addClass('form-field__textarea');
-    $('label').addClass('form-field__label');*/
-    $('.input-contrast').addClass('form-control').parent().addClass('form-group');
-    //$('.input-contrast, label').wrapAll('<div class="form-group"></div>');
+    $('.input-contrast:not(.header-search-input)').addClass('form_control');
+    $('label + .input-contrast:not(.header-search-input)').wrapAll('<div class="form_group"></div>');
 
     // INPUTS ACTIVATION
     $(document).ready(function() {
-        $(".form-control").click(function() {
+        $(".form_control").click(function() {
             $(this).parent().addClass("label-animate");
         });
 
         $(window).click(function() {
-            if (!$(event.target).is('.form-control')) {
-                $(".form-control").each(function() {
+            if (!$(event.target).is('.form_control')) {
+                $(".form_control").each(function() {
                     if ($(this).val() == '') {
                         $(this).parent().removeClass("label-animate");
                     }
